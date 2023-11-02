1c35fe9529afc7663d3fda7fe7626239d6a57321
danieleardizzone <daniele.ardi02@gmail.com>
2023-11-02 17:43:38 +0100

    ADD - bonus

--- js/app.js
+++ js/app.js
@@ -5,18 +5,20 @@ console.log(axios)
 createApp({
   data() {
     return {
-      count: 0
+      emails: [],
     }
   }, methods: {
     getRandomEmail(){
-      for(i = 0; i < 10; i++){
-      axios
-        .get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then((res) => {
-          console.log(res.data.response)
+      for (i = 0; i < 10; i++){
+        axios
+          .get('https://flynn.boolean.careers/exercises/api/random/mail')
+          .then((res) => {
+            // console.log(res.data.response)
+            const getEmail = res.data.response
+            this.emails.push(getEmail)
 
-          
-        })
+            // console.log(this.emails)
+          })
       }
     }
   }, created() {
